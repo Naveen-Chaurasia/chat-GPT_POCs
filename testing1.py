06d6c3df90520ebd69b2027131ef8de388796f8a
Naveen <naveen.chaurasia861@gmail.com>
2023-03-08 10:58:20 +0530

    converted main2.py to flask project and added sum of gwp

--- testing1.py
+++ testing1.py
@@ -1,12 +1,18 @@
-import asyncio
-from chatgpt_wrapper import AsyncChatGPT
+# import asyncio
+# from chatgpt_wrapper import AsyncChatGPT
 
-async def main():
-    agpt = AsyncChatGPT()
-    await agpt.create()
-    await agpt.refresh_session()
-    response = await agpt.ask("Hello, world!")
-    print(response)
-    # Hello there! How can I assist you today?
+# async def main():
+#     agpt = AsyncChatGPT()
+#     await agpt.create()
+#     await agpt.refresh_session()
+#     response = await agpt.ask("Hello, world!")
+#     print(response)
+#     # Hello there! How can I assist you today?
 
-asyncio.run(main())
+# asyncio.run(main())
+
+from chatgpt_wrapper import ChatGPT
+
+bot = ChatGPT()
+response = bot.ask("Hello, world!")
+print(response)  # prints the response from chatGPT
